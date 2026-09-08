df58b04172b620c70c48f4dbba0f0b50136b02b1
Galaxys22702 <109320424+Galaxys22702@users.noreply.github.com>
2026-09-07 21:29:48 -0700

    Redesign waitlist with Streetwise brand

--- src/server.js
+++ src/server.js
@@ -42,7 +42,8 @@ const mimeTypes = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
-  ".json": "application/json; charset=utf-8"
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png"
 };
 
 function applySecurityHeaders(res) {
